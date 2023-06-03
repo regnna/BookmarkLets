89189d574597230e9f7639270140d85a47e4315d
Ritam Chatterjee <85054086+regnna@users.noreply.github.com>
2023-06-03 21:18:43 +0530

    Create Page_Editable.js
    IT is woring

---
+++ Page_Editable.js
@@ -0,0 +1,5 @@
+javascript: 
+document.body.contentEditable = true; 
+let all_links = document.getElementsByTagName("a");  
+for(let i=0; i<all_links.length; i++){     all_links[i].removeAttribute("href"); } 
+void 0;
